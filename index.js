12f83e9883d190b862b6a59421eba37e2f7d822e
vilianap <vilianappp@gmail.com>
2023-08-20 18:42:50 +0100

    updated js css and html

--- index.js
+++ index.js
@@ -21,6 +21,29 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
